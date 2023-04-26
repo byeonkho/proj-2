04a7d15129f1ce998fe3c94d577a06cb6aafcabd
byeonkho <byeon.kho@gmail.com>
2023-04-26 23:15:05 +0800

    working per capita toggle and data transform

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -10,15 +10,26 @@ const LineChart = (props) => {
             },
             xaxis: {
                 categories: [],
-                max: 50,
             },
             legend: {
                 show: true,
                 position: "top",
-                horizontalAlign: "right",
+                horizontalAlign: "center",
                 labels: {
                     colors: "#333",
-                    useSeriesColors: false,
+                    useSeriesColors: true,
+                },
+            },
+            yaxis: {
+                min: 0,
+                labels: {
+                    formatter: function (val) {
+                        if (val >= 1) {
+                            return Math.round(val);
+                        } else {
+                            return val;
+                        }
+                    },
                 },
             },
         },
@@ -28,9 +39,6 @@ const LineChart = (props) => {
                 data: [],
             },
         ],
-        yaxis: {
-            min: 0,
-        },
     });
 
     useEffect(() => {
@@ -42,17 +50,43 @@ const LineChart = (props) => {
             const data = props.countriesData[i];
             const country = data.Country;
 
-            if (!countryNames.includes(country)) {
-                countryNames.push(country);
+            if (!countryNames.some((obj) => obj.Country === country)) {
+                const populationObj = countries.find(
+                    (obj) => obj.Country === country
+                );
+
+                const dataObj = {
+                    Country: country,
+                    Population: populationObj.population,
+                };
+
+                countryNames.push(dataObj);
 
                 const newData = { name: country, data: [] };
                 series.push(newData);
             }
 
-            for (let j = 0; j < series.length; j++) {
-                if (series[j].name === country) {
-                    series[j].data.push(data.New);
-                    break;
+            if (props.capitaState === true) {
+                for (let j = 0; j < series.length; j++) {
+                    if (series[j].name === country) {
+                        // find the population value in the countryNames array that matches the current country
+                        const population = countryNames.find(
+                            (obj) => obj.Country === country
+                        ).Population;
+                        if (population !== 0 && population !== undefined) {
+                            series[j].data.push(data.New / population);
+                        } else {
+                            series[j].data.push(0);
+                        }
+                        break;
+                    }
+                }
+            } else if (props.capitaState === false) {
+                for (let j = 0; j < series.length; j++) {
+                    if (series[j].name === country) {
+                        series[j].data.push(data.New);
+                        break;
+                    }
                 }
             }
         }
@@ -61,19 +95,13 @@ const LineChart = (props) => {
 
         setChartData({
             options: {
-                chart: {
-                    id: "basic-line",
-                },
                 xaxis: {
                     categories: categories,
                 },
             },
             series: series,
-            yaxis: {
-                min: 0,
-            },
         });
-    }, [props.countriesData]);
+    }, [props.countriesData, props.capitaState]);
 
     return (
         <div>
